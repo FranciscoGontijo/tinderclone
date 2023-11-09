1fce520c49e0e995e052c9d71cff3cb02adbf7b1
FranciscoGontijo <franciscoacmg@gmail.com>
2023-11-09 14:13:18 -0300

    Creating setUser to login and logout

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,34 +1,38 @@
-import React, { createContext, useContext, ReactNode } from 'react';
+import React, { createContext, useContext, ReactNode, useState } from 'react';
 import { View, Text } from 'react-native';
 
 interface AuthContextType {
-    user: string;
-  }
-  
-  const initialContextValue: AuthContextType = {
-    user: '',
-  };
-  
-  const AuthContext = createContext<AuthContextType>(initialContextValue);
-  
-  type AuthProviderProps = {
+    user: string | null;
+    setUser: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+const initialContextValue: AuthContextType = {
+    user: null,
+    setUser: () => { },
+};
+const AuthContext = createContext<AuthContextType>(initialContextValue);
+
+type AuthProviderProps = {
     children: ReactNode;
-  };
-  
-  export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+};
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+    const [user, setUser] = useState<string | null>(null);
+
     return (
-      <AuthContext.Provider value={{
-        user: "Francisco",
-      }}>
-        {children}
-      </AuthContext.Provider>
+        <AuthContext.Provider value={{
+            user,
+            setUser,
+        }}>
+            {children}
+        </AuthContext.Provider>
     );
-  };
-  
-  export default function useAuth(): AuthContextType {
+};
+
+export default function useAuth(): AuthContextType {
     const context = useContext(AuthContext);
     if (context === undefined) {
-      throw new Error('useAuth must be used within an AuthProvider');
+        throw new Error('useAuth must be used within an AuthProvider');
     }
     return context;
-  }
+};
