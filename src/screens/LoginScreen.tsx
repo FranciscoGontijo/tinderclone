83831b835e38d4398a3ce085fda3822f9afc7cae
FranciscoGontijo <franciscoacmg@gmail.com>
2023-11-09 14:15:00 -0300

    Login Screen with login function

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,16 +1,47 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
 import useAuth from '../../hooks/useAuth';
 
 const LoginScreen = () => {
-  const { user } = useAuth();
+  const { user, setUser } = useAuth();
+
+  const navigation = useNavigation();
+
+  const login = () => {
+    setUser('Francisco');
+  };
 
   return (
-    <View>
-        <Text>This is Login Screen</Text>
+    <View style={{ flex: 1, }}>
+      <ImageBackground
+        source={{ uri: "http://tinder.com/static/tinder.png" }}
+        resizeMode='cover'
+        style={{ flex: 1, }}>
+        <TouchableOpacity 
+          style={styles.signInButton}
+          onPress={login}>
+          <Text style={styles.buttonText}>Sign in & get swiping</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   )
 };
 
 export default LoginScreen;
 
+const styles = StyleSheet.create({
+  signInButton: {
+    position: 'absolute',
+    bottom: 40,
+    width: 200,
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    padding: 12,
+    borderRadius: 14,
+  },
+  buttonText: {
+    textAlign: 'center',
+
+  }
+})
