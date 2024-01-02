3adc2a20aaf8728d75c781b0f627f69dbaf8c3ed
FranciscoGontijo <franciscoacmg@gmail.com>
2024-01-02 08:42:55 -0300

    Matched users screen CSS and header, clickable user view to go to chat screen passing id to fetch chat

--- src/screens/MatchedUsersScreen.tsx
+++ src/screens/MatchedUsersScreen.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, FlatList } from 'react-native';
+import { View, Text, Button, FlatList, SafeAreaView, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import api from '../services/api';
+import { Ionicons } from '@expo/vector-icons';
 
+import api from '../services/api';
 import useAuth from '../../hooks/useAuth';
 
 type MatchedUserType = {
     name: string;
     photoUrl: string;
+    _id: string
 }
 
-const MatchedUsersScreen = () => {
+const MatchedUsersScreen: React.FC = () => {
     const [matchedList, setMatchedList] = useState<MatchedUserType[] | null>(null);
     const [loading, setLoading] = useState<Boolean>(true);
 
@@ -29,9 +31,9 @@ const MatchedUsersScreen = () => {
                         'Authorization': 'Bearer ' + token
                     }
                 });
+                console.log(response.data);
                 setMatchedList(response.data);
                 setLoading(false);
-                console.log(response.data);
             } catch (error) {
                 console.log(error);
             }
@@ -44,17 +46,80 @@ const MatchedUsersScreen = () => {
         }
     }, []);
 
+
     return (
-        <View>
+        <SafeAreaView>
+            <View style={styles.header}>
+
+                <TouchableOpacity onPress={signOut} >
+                    {user && <Image
+                        style={styles.profileImage}
+                        source={{ uri: user.photoUrl }} />
+                    }
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                    <Image
+                        source={require('../../assets/logo.png')}
+                        style={styles.logoImage}
+                        resizeMode="contain"
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => navigation.navigate('Matched')}>
+                    <Ionicons name="chatbubbles-sharp" size={30} color="#FF5864" />
+                </TouchableOpacity>
+
+            </View>
+
             {matchedList && <FlatList
                 data={matchedList}
                 renderItem={({ item }) => (
-                    <Text>{item.name}</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('Chat', { userId: item._id})}>
+                        <View style={styles.chatContainer}>
+                            <Image
+                                style={styles.chatImage}
+                                source={{ uri: item.photoUrl }} />
+                            <Text>{item.name}</Text>
+                        </View>
+                    </TouchableOpacity>
                 )} />}
-            <Text>Matched Screen</Text>
-            <Button title="Back Home" onPress={() => navigation.navigate('Home')} />
-        </View>
+
+        </SafeAreaView>
     )
 };
 
-export default MatchedUsersScreen
+export default MatchedUsersScreen;
+
+
+const styles = StyleSheet.create({
+    profileImage: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+    },
+    header: {
+        height: 70,
+        marginTop: 25,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+    logoImage: {
+        height: 45,
+        width: 40,
+    },
+    chatContainer: {
+        height: 60,
+        border: '1px solid black',
+        display: 'flex',
+        flexDirection: 'row',
+        marginTop: 30,
+    },
+    chatImage: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        marginLeft: 20
+    }
+});
