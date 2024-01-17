12fd526e2f98a5439c83ce93c6ea4ca8e3e6840d
FranciscoGontijo <franciscoacmg@gmail.com>
2024-01-17 17:58:48 -0300

    Using modules to make code cleaner at Chat Screen (all http request now are at api.ts). import Header component

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,16 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { ActivityIndicator, View, Text, SafeAreaView, StyleSheet, Image, TouchableOpacity, FlatList, TextInput } from 'react-native';
+import { ActivityIndicator, View, Text, SafeAreaView, StyleSheet, Image, TouchableOpacity, FlatList, TextInput, KeyboardAvoidingView } from 'react-native';
 import { useNavigation, RouteProp } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
 
-import api from '../services/api';
+import { MessageType, fetchChatMessages, updateChatAtDatabase } from '../services/api';
 import useAuth from '../../hooks/useAuth';
 
-type MessageType = {
-    name: string,
-    message: string,
-    userId: string
-}
+//import components
+import Header from '../components/Header';
+
 
 type ChatScreenRouteProp = RouteProp<{ Chat: { userId: string } }, 'Chat'>;
 
@@ -22,15 +19,15 @@ const ChatScreen: React.FC<ChatScreenProps> = ({ route }) => {
     const [chat, setChat] = useState<MessageType[]>([]);
     const [newMessage, setNewMessage] = useState<string>('');
     const [loading, setLoading] = useState<Boolean>(true);
+    //Auth Hook
+    const { user, token, socket } = useAuth();
 
-    const { user, logOut, token, socket } = useAuth();
-
+    //Use Ref to update database just when needed
     const chatRef = useRef(chat);
-
     let messagesCounter: number = 0;
 
+    //UserId is the matched user that 
     const { userId } = route.params;
-
     const roomId: string = [user?._id.toString(), userId].sort().join('-');
 
     const navigation = useNavigation();
@@ -42,35 +39,11 @@ const ChatScreen: React.FC<ChatScreenProps> = ({ route }) => {
         chatRef.current = chat;
     }, [chat]);
 
-
-    const updateChatAtDatabase = async (updatedChat: MessageType[]) => {
-        try {
-            const response = await api.post(`/chatupdate/${userId}`, { chat: updatedChat }, {
-                signal: controller.signal,
-                headers: {
-                    'Authorization': 'Bearer ' + token
-                }
-            });
-            console.log(response.data); // Assuming you want to return something from the API call
-        } catch (error: any) {
-            if (error.name === 'AbortError') {
-                // Handle the cancellation error if needed
-                console.log('Request canceled:', error.message);
-            } else if (error.name === 'TypeError' && error.message === 'Failed to fetch') {
-                // Handle network errors or failed fetch request
-                console.log('Network error:', error.message);
-            } else {
-                // Handle other API errors
-                console.log('API error:', error.message);
-            }
-        }
-    };
-
     const startDatabaseUpdates = () => {
         //create an if that check if there is new messages
         if (chatRef.current.length > messagesCounter) {
             let newChatArray = chatRef.current;
-            updateChatAtDatabase(newChatArray);
+            updateChatAtDatabase(userId, controller, token, newChatArray);
             messagesCounter = chatRef.current.length;
         } else {
             console.log('Not new messages, not gonna update');
@@ -80,24 +53,9 @@ const ChatScreen: React.FC<ChatScreenProps> = ({ route }) => {
 
     useEffect(() => {
 
-        const fetchChatMessages = async () => {
-            try {
-                const response = await api.get(`/chat/${userId}`, {
-                    signal: controller.signal,
-                    headers: {
-                        'Authorization': 'Bearer ' + token
-                    }
-                });
-                console.log('Fetching chat' + response.data);
-                setChat(response.data);
-                setLoading(false);
-            } catch (error) {
-                console.log(error);
-                throw error;
-            }
-        };
         setLoading(true);
-        fetchChatMessages();
+
+        fetchChatMessages(userId, controller, token, setChat, setLoading);
 
         //Use socket.io to handle messages
         socket?.emit('openChat', { senderId: user?._id.toString(), recipientId: userId });
@@ -110,7 +68,7 @@ const ChatScreen: React.FC<ChatScreenProps> = ({ route }) => {
         startDatabaseUpdates();
 
         const cleanup = async () => {
-            await updateChatAtDatabase(chatRef.current);
+            await updateChatAtDatabase(userId, controller, token, chatRef.current);
             controller.abort();
             clearTimeout(updateChatTimeout);
         };
@@ -137,55 +95,40 @@ const ChatScreen: React.FC<ChatScreenProps> = ({ route }) => {
 
     return (
         <SafeAreaView>
-            <View style={styles.header}>
 
-                <TouchableOpacity onPress={logOut} >
-                    {user && <Image
-                        style={styles.profileImage}
-                        source={{ uri: user.photoUrl }} />
-                    }
-                </TouchableOpacity>
+            <Header />
 
-                <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-                    <Image
-                        source={require('../../assets/logo.png')}
-                        style={styles.logoImage}
-                        resizeMode="contain"
-                    />
-                </TouchableOpacity>
+            <View style={styles.chatView}>
 
-                <TouchableOpacity onPress={() => navigation.navigate('Matched')}>
-                    <Ionicons name="chatbubbles-sharp" size={30} color="#FF5864" />
-                </TouchableOpacity>
+                {chat && <FlatList
+                    data={chat}
+                    renderItem={({ item }) => {
+                        //you can update the variable here to create the blocks.
+                        // if message is from the same user that last message was. push inside a block
+                        //render the block instead of the item.message
 
+                        if (user?._id === item.userId) {
+                            return (
+                                <View style={styles.chatBubbleRight}>
+                                    <Text style={styles.chatText}>{item.message}</Text>
+                                </View>
+                            )
+                        } else {
+                            return (
+                                <View style={styles.chatBubbleLeft}>
+                                    <Text style={styles.chatText}>{item.message}</Text>
+                                </View>
+                            )
+                        }
+                    }}
+                />}
+                
             </View>
-            {chat && <FlatList
-                data={chat}
-                renderItem={({ item }) => {
-                    //you can update the variable here to create the blocks.
-                    // if message is from the same user that last message was. push inside a block
-                    //render the block instead of the item.message
 
-                    if (user?._id === item.userId) {
-                        return (
-                            <View style={styles.chatBubbleRight}>
-                                <Text style={styles.chatText}>{item.message}</Text>
-                            </View>
-                        )
-                    } else {
-                        return (
-                            <View style={styles.chatBubbleLeft}>
-                                <Text style={styles.chatText}>{item.message}</Text>
-                            </View>
-                        )
-                    }
-                }}
-            />}
             <View>
 
                 <TextInput
                     style={styles.textInput}
-                    multiline={true}
                     onChangeText={setNewMessage}
                     value={newMessage}
                     onSubmitEditing={() => sendMessage()}
@@ -200,24 +143,11 @@ const ChatScreen: React.FC<ChatScreenProps> = ({ route }) => {
 export default ChatScreen;
 
 const styles = StyleSheet.create({
-    profileImage: {
-        width: 40,
-        height: 40,
-        borderRadius: 20,
-    },
-    header: {
-        height: 70,
-        marginTop: 25,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 10,
-    },
-    logoImage: {
-        height: 45,
-        width: 40,
+    chatView: {
+        //Need to apply scroll to chat view and set on max height
     },
     chatText: {
-        fontSize: 16,
+        fontSize: 20,
     },
     chatBubbleRight: {
         backgroundColor: 'lightblue',
