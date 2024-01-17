eaade06d4e0e8c90e7fa51cec924a4aabba256a8
FranciscoGontijo <franciscoacmg@gmail.com>
2024-01-17 18:03:39 -0300

    Rename UserType

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,6 @@
 import api from "./api";
 
-export type userType = {
+export type UserType = {
     name: string;
     email: string;
     photoUrl: string;
@@ -10,11 +10,11 @@ export type userType = {
 
 export type LoginType = {
     token: string | null,
-    user: userType | null
+    user: UserType | null
 }
 
 export const signUp = async (name: string, email: string, password: string): Promise<LoginType | null> => {
-    let user: userType | null = null;
+    let user: UserType | null = null;
     let token: string | null = null;
     console.log(name, email, password);
     //use Axios to make the request
@@ -35,7 +35,7 @@ export const signUp = async (name: string, email: string, password: string): Pro
 
 export const logIn = async (email: string, password: string): Promise<LoginType | null> => {
     let token: string | null = null
-    let user: userType | null = null
+    let user: UserType | null = null
     await api.post('/login', { email: email, password: password })
         .then((response) => {
             token = response.data.accessToken;  
@@ -46,3 +46,4 @@ export const logIn = async (email: string, password: string): Promise<LoginType 
         });
     return {token, user};
 };
+
