75be353c01437bed66bd235d9c4e0686b12aa54e
FranciscoGontijo <franciscoacmg@gmail.com>
2024-01-18 15:27:09 -0300

    Sign up form CSS

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, Button, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-
+import { View, Text, Pressable, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 interface SignUpPropsType {
     handleFormChange: () => void;
@@ -22,31 +22,52 @@ const SignUpForm: React.FC<SignUpPropsType> = (props): JSX.Element => {
     return (
         <View style={styles.container} >
 
-            <TextInput
-                placeholder='name'
-                onChangeText={setUserName}
-                onFocus={() => setUserNameAlert(false)}
-            />
+            <View style={styles.inputContainer}>
+                <Text style={{ fontSize: 23 }}>Your Name</Text>
+                <View style={styles.iconContainer}>
+                    <Ionicons style={{ marginRight: 4 }} name='person' size={20} />
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder='Your name'
+                        onChangeText={setUserName}
+                        onFocus={() => setUserNameAlert(false)}
+                    />
+                </View>
+            </View>
 
             {userNameAlert && <Text style={{ color: 'red' }}>Name required</Text>}
 
-            <TextInput
-                placeholder="email"
-                onChangeText={setUserEmail}
-                onFocus={() => setEmailAlert(false)}
-            />
+            <View style={styles.inputContainer}>
+                <Text style={{ fontSize: 23 }}>Email Address</Text>
+                <View style={styles.iconContainer}>
+                    <Ionicons style={{ marginRight: 4 }} name='mail-outline' size={20} />
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="youremail@email.com"
+                        onChangeText={setUserEmail}
+                        onFocus={() => setEmailAlert(false)}
+                    />
+                </View>
+            </View>
 
             {emailAlert && <Text style={{ color: 'red' }}>Email required</Text>}
 
-            <TextInput
-                placeholder="password"
-                onChangeText={setPassword}
-                onFocus={() => setPasswordAlert(false)}
-            />
+            <View style={styles.inputContainer}>
+                <Text style={{ fontSize: 23 }}>Email Address</Text>
+                <View style={styles.iconContainer}>
+                    <Ionicons style={{ marginRight: 4 }} name='lock-closed-outline' size={20} />
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder="password"
+                        onChangeText={setPassword}
+                        onFocus={() => setPasswordAlert(false)}
+                    />
+                </View>
+            </View>
 
             {passwordAlert && <Text style={{ color: 'red' }}>Password required</Text>}
 
-            <Button title="Sign In" onPress={() => {
+            <Pressable style={styles.signInButton} onPress={() => {
                 if (!userName) {
                     setUserNameAlert(true)
                 }
@@ -59,11 +80,14 @@ const SignUpForm: React.FC<SignUpPropsType> = (props): JSX.Element => {
                 if (userName && userEmail && password) {
                     handleSubmit(userName, userEmail, password)
                 }
-            }} />
+            }} >
+                <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white'  }}>Sign In</Text>
+            </Pressable>
 
             <TouchableOpacity
+                style={{ marginTop: 20 }}
                 onPress={handleFormChange}>
-                <Text>Back to Log in</Text>
+                <Text style={{ fontSize: 18 }}>Back to Log in</Text>
             </TouchableOpacity>
 
         </View>
@@ -76,5 +100,33 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'pink'
     },
+    inputContainer: {
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        backgroundColor: 'white',
+        marginBottom: 30,
+        width: '80%',
+        borderRadius: 20,
+        padding: 20,
+    },
+    iconContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    textInput: {
+        fontSize: 20
+    },
+    signInButton: {
+        height: 50,
+        width: '80%',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "#FF5864",
+        color: 'white'
+
+    }
 });
