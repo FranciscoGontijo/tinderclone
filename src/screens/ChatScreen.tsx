adcad67113e2313122a5c369b0e89855a4c0eda6
FranciscoGontijo <franciscoacmg@gmail.com>
2024-01-19 15:15:09 -0300

    Chat screen CSS, and new header

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -9,10 +9,11 @@ import {
     TextInput,
     KeyboardAvoidingView,
     Platform,
-    ScrollView
+    Image
 } from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { RouteProp } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 
 import { MessageType, fetchChatMessages, updateChatAtDatabase } from '../services/api';
 import useAuth from '../../hooks/useAuth';
@@ -20,7 +21,7 @@ import useAuth from '../../hooks/useAuth';
 //import components
 import Header from '../components/Header';
 
-type ChatScreenRouteProp = RouteProp<{ Chat: { userId: string } }, 'Chat'>;
+type ChatScreenRouteProp = RouteProp<{ Chat: { userId: string, userName: string, photoUrl: string } }, 'Chat'>;
 
 type ChatScreenProps = {
     route: ChatScreenRouteProp;
@@ -38,10 +39,11 @@ const ChatScreen: React.FC<ChatScreenProps> = ({ route }) => {
     let messagesCounter: number = 0;
 
     //UserId is the matched user that 
-    const { userId } = route.params;
+    const { userId, userName, photoUrl } = route.params;
     const roomId: string = [user?._id.toString(), userId].sort().join('-');
 
     const controller = new AbortController();
+    const navigation = useNavigation();
 
     let updateChatTimeout: NodeJS.Timeout;
 
@@ -55,7 +57,7 @@ const ChatScreen: React.FC<ChatScreenProps> = ({ route }) => {
             let newChatArray = chatRef.current;
             updateChatAtDatabase(userId, controller, token, newChatArray);
             messagesCounter = chatRef.current.length;
-        } 
+        }
         updateChatTimeout = setTimeout(() => startDatabaseUpdates(), 20000);
     };
 
@@ -76,7 +78,9 @@ const ChatScreen: React.FC<ChatScreenProps> = ({ route }) => {
         startDatabaseUpdates();
 
         const cleanup = async () => {
-            await updateChatAtDatabase(userId, controller, token, chatRef.current);
+            if (chatRef.current.length > messagesCounter) {
+                await updateChatAtDatabase(userId, controller, token, chatRef.current);
+            }
             controller.abort();
             clearTimeout(updateChatTimeout);
         };
@@ -102,9 +106,16 @@ const ChatScreen: React.FC<ChatScreenProps> = ({ route }) => {
     };
 
     return (
-        <SafeAreaView style={{flex: 1,}}>
+        <SafeAreaView style={{ flex: 1, }}>
 
-            <Header />
+            <View style={styles.header}>
+                <Ionicons onPress={() => navigation.goBack()} name='chevron-back' size={45} color='#67667b' />
+                <Image
+                    style={styles.profileImage}
+                    source={{ uri: photoUrl }} />
+
+                <Text style={{ fontSize: 28, fontWeight: 'bold', color:'#67667b' }}>{userName}</Text>
+            </View>
 
             <View style={styles.chatView}>
 
@@ -141,11 +152,11 @@ const ChatScreen: React.FC<ChatScreenProps> = ({ route }) => {
                     value={newMessage}
                     onSubmitEditing={() => sendMessage()}
                     placeholder='Type a message'
+                    placeholderTextColor='#484759'
                 />
 
+                <Ionicons color='#484759' name='send' size={30} />
             </KeyboardAvoidingView>
-
-            <View style={styles.chatFooter} />
 
         </SafeAreaView>
     )
@@ -155,55 +166,74 @@ export default ChatScreen;
 
 const styles = StyleSheet.create({
     chatView: {
-        //Need to apply scroll to chat 
         flex: 1,
-
+        backgroundColor: '#13101c',
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop: 20,
+        paddingBottom: 10,
+        paddingRight: 30,
+        paddingLeft: 10,
+        backgroundColor: '#13101c',
+    },
+    profileImage: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
     },
     chatText: {
         fontSize: 20,
+        color: 'white'
     },
     chatBubbleRight: {
-        backgroundColor: 'lightblue',
+        backgroundColor: '#3aa3f2',
         borderBottomRightRadius: 2,
         borderTopRightRadius: 10,
-        borderTopLeftRadius: 10,
-        borderBottomLeftRadius: 10,
+        borderTopLeftRadius: 20,
+        borderBottomLeftRadius: 20,
         marginBottom: 4,
         marginRight: 14,
         marginLeft: 44,
         alignSelf: 'flex-end',
-        padding: 5,
+        padding: 6,
+        paddingLeft: 12,
+        paddingRight: 8,
 
     },
     chatBubbleLeft: {
-        backgroundColor: 'lightgray',
-        borderBottomRightRadius: 10,
-        borderTopRightRadius: 10,
+        backgroundColor: '#29282c',
+        borderBottomRightRadius: 20,
+        borderTopRightRadius: 20,
         borderTopLeftRadius: 10,
         borderBottomLeftRadius: 2,
         marginBottom: 4,
         marginLeft: 14,
         marginRight: 44,
         alignSelf: 'flex-start',
-        padding: 5,
+        padding: 6,
+        paddingRight: 14,
+        paddingLeft: 12,
     },
     textInputContainer: {
         padding: 10,
-        backgroundColor: '#fff',
-        borderColor: '#666',
-        borderTopWidth: 1,
+        backgroundColor: '#13101c',
         flexDirection: 'row',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
     },
     textInput: {
         height: 36,
-        width: '100%',
+        width: '90%',
         borderWidth: 1,
+        borderColor: '#484759',
         borderRadius: 18,
         padding: 10,
         fontSize: 16,
         alignSelf: 'center',
+        color: '#484759',
     },
     chatFooter: {
         height: 36,
